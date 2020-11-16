3ed69659ef24da7b0ce063582e87b0155146a4e1
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-11-16 17:56:57 +0530

    Added:Code for generating invoice summary

---
+++ src/main/java/com/blz/invoicegenerator/InvoiceGenerator.java
@@ -0,0 +1,31 @@
+package com.blz.invoicegenerator;
+
+public class InvoiceGenerator {
+
+	private static final double Min_Cost_Per_KiloMeter = 10;
+	private static final int Cost_per_Time = 1;
+	private static final double Minimum_Fare = 5;
+
+	public double calculareFare(double distance, double time) {
+		double cost = distance * Min_Cost_Per_KiloMeter + time * Cost_per_Time;
+		if (cost < Minimum_Fare)
+			return Minimum_Fare;
+		return cost;
+	}
+
+	public double calculareFareForMultipleRides(Ride[] rides) {
+		double cost = 0;
+		for (Ride ride : rides) {
+			cost = cost + this.calculareFare(ride.distance, ride.time);
+		}
+		return cost;
+	}
+
+	public Invoice_Summary calculareFareSummary(Ride[] rides) {
+		double cost = 0;
+		for (Ride ride : rides) {
+			cost = cost + this.calculareFare(ride.distance, ride.time);
+		}
+		return new Invoice_Summary(rides.length, cost);
+	}
+}
